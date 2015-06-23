e0be676037387b12a1d0bb75ee71097f379706e8
lchsk <mjlechowski@gmail.com>
2015-06-23 13:15:31 +0100

    hardware specs and wordcount

--- src/phi/openmp/wordcount/main.cpp
+++ src/phi/openmp/wordcount/main.cpp
@@ -3,55 +3,57 @@
 #include <fstream>
 #include <map>
 #include <vector>
-#include <typeinfo>
 #include <cmath>
-// #include <stdio>
-// #include <stdlib.h>
-// #include <unistd.h>
-
-
-using namespace std;
 
 int
 main (int argc, char* argv[])
 {
-    std::map<string, int> words;
-    std::map<string, int> shwords;
-    vector<string> v_words;
+    // private
+    std::map<std::string, int> words_p; 
+    
+    // shared
+    std::map<std::string, int> words_sh;
+    
+    // vector with all words
+    std::vector<std::string> v_words;
 
-    ifstream file ("great-expectations.txt");
-    string word;
+    if (argv[1] == NULL)
+    {
+        std::cout << "Argument needed: text file" << std::endl;
+        exit(0);
+    }
+    
+    std::ifstream file (argv[1]);
+    std::string word;
+    double t;
 
-
-    double start = omp_get_wtime();
-    // cout << start << endl;
+    t = omp_get_wtime();
 
     if (file.is_open())
     {
         while (file >> word)
-        // while (getline (myfile, line))
         {
-            // cout << word << endl;
-            // if (words.find(word) == words.end())
-                // words[word] = 1;
-            // else
-                // words[word]++;
-            // cout << typeid(word).name() << endl;
             v_words.push_back(word);
         }
 
         file.close();
-
     }
-    else 
-        cout << "Unable to open file";
+
+    std::cout << "Time of reading file: " << (omp_get_wtime() - t) * 1000 << " ms " << std::endl;
+    t = omp_get_wtime();
 
     int th_id, nthreads;
 
-    cout << "Words: " << v_words.size() << endl;
-    nthreads = 4;
+    std::cout << "Words: " << v_words.size() << std::endl;
+
+    if (argv[2] == NULL)
+        nthreads = omp_get_max_threads();
+    else
+        nthreads = atoi(argv[2]);
+
+    std::cout << "Using " << nthreads << " threads. " << std::endl;
+
     int part_size = ceil(v_words.size() / (float) nthreads);
-    cout << "size: " << part_size << endl;
 
     #pragma omp sections
     {
@@ -59,63 +61,30 @@ main (int argc, char* argv[])
         {
             omp_set_num_threads(nthreads);
 
-            #pragma omp parallel private (th_id, words) shared (v_words, shwords)
+            #pragma omp parallel private (th_id, words_p) shared (v_words, words_sh)
             {
                 th_id = omp_get_thread_num();
-                // printf ("Thread %d\n", th_id);
-                // cout << "start: " << th_id * part_size << " end: " << th_id * part_size + part_size << endl;
 
                 int start = th_id * part_size;
                 int end = start + part_size;
-                int m = min(end, (int) v_words.size());
-                // cout << "Min: " << m << endl;
+                int m = std::min(end, (int) v_words.size());
 
-                // cout << "s: " << start << " e: " << m << endl;
                 for (int i = start; i < m; i++)
                 {
-                    // cout << v_words[i] << endl;
-                    // if (i < v_words.size())
-                        
-                        // if (words.find(s) == words.end())
-                        //     words[s] = 1;
-                        // else
-                        //     words[s]++;
-                            string s = v_words[i];
-                            // #pragma omp atomic
-                            words[s]++;
-                            // #pragma omp flush (words[s])
-                            // words["do, 22"]++;
-
-                    // words[v_words[i]]++;
+                    std::string s = v_words[i];
+                    words_p[s]++;
                 }
 
                 #pragma omp critical
-                for(auto elem : words)
+                for(auto i : words_p)
                 {
-                    shwords[elem.first] += elem.second;
-                   // std::cout << elem.first << " " << elem.second << "\n";
+                    words_sh[i.first] += i.second;
                 }
-
-
-                // if (th_id == 0)
-                // {
-                //     printf ("Number of threads = %d\n", omp_get_num_threads());
-                // }
-
             }
         }
     }
 
-    // for(auto elem : shwords)
-    // {
-    //    std::cout << elem.first << " " << elem.second << "\n";
-    // }
-
-    double end = omp_get_wtime();
-    // cout << end << endl;
-
-    // cout << ((double) end - (double) start) * 1000.0 / CLOCKS_PER_SEC << endl;
-    cout << "Time: " << (end - start) * 1000 << endl;
+    std::cout << "Time of counting: " << (omp_get_wtime() - t) * 1000 << " ms " << std::endl;
 
     return 0;
 }
